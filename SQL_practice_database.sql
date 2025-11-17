ac0ad5b5720125f4de7044c0aa65bddaca673300
Wasay Ghauri <wasayghauri514@gmail.com>
2025-11-17 23:59:43 +0500

    Add files via upload

---
+++ SQL_practice_database.sql
@@ -0,0 +1,29 @@
+create table Product_list(
+product_id int8 primary key,
+product_name TEXT,
+Category TEXT, 
+Price INT,
+stock INT 
+)
+
+
+select * from product_list
+
+
+insert into product_list
+(product_id, product_name, Category, Price, stock)
+values
+
+(001,'Laptop','Electronics',45000,15),
+(002,'mobile','Electronics',35000,19),
+(003,'Fridge','Electronics',55000,10),
+(004,'Television','Electronics',26000,10),
+(005,'Washing_machine','Electronics',42000,11),
+(006,'Chair','Furniture',10000,18),
+(007,'Laptop','Electronics',35000,17),
+(008,'Shoes','Fashion',5000,12),
+(009,'Laptop','Electronics',65000,13),
+(010,'Backpack','Accessories',2000,19);
+
+
+
