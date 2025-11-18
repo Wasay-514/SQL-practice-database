c937d33d226a717d391b1edaaee6d17581e84fc2
Wasay Ghauri <wasayghauri514@gmail.com>
2025-11-18 23:59:57 +0500

    Add files via upload

---
+++ first_quiery.sql
@@ -0,0 +1,40 @@
+-- CREATE TABLE customer(
+-- "id" int PRIMARY KEY,
+-- "name" varchar(50) NOT NULL,
+-- "age" int NOT NULL,
+-- "city" char(50),
+-- "salary" numeric
+-- );
+
+
+
+-- INSERT INTO customer
+-- (ID, Name, Age, City, Salary)
+-- VALUES
+-- (1, 'wasay', 23, 'Karachi', 850000),
+-- (2, 'Tariq', 49, 'Karachi', 850000),
+-- (3, 'Sara', 20, 'Lahore', 850000),
+-- (4, 'Aqsa', 21, 'Karachi', 850000)
+
+
+
+-- UPDATE customer
+-- SET salary = 45000
+-- WHERE ID  = 4;
+
+
+
+
+
+SELECT * FROM customer
+
+-- SELECT name FROM customer
+
+-- SELECT DISTINCT id FROM customer
+
+
+
+
+
+
+
